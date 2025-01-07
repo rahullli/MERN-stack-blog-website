a8cedd011be5f693cfcdfdec09ec4e563ee72f73
Rahullli <khandelwalr213@gmail.com>
2025-01-07 23:06:09 +0530

    Added logic for home page post.

--- src/components/inpage-navigation.component.jsx
+++ src/components/inpage-navigation.component.jsx
@@ -1,29 +1,39 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
-const InPageNavigationComponent = ({routes}) => {
-    const [inPageNavIndex, setInPageNavIndex] = useState(0);
+const InPageNavigationComponent = ({routes, defaultActiveIndex = 0, defaultHidden = [], children}) => {
     let activeTabLineRef = useRef();
-    const changePageState = () =>{
-        
+    let activeTabRef = useRef();
+    const [inPageNavIndex, setInPageNavIndex] = useState(0);
+
+    const changePageState = (btn,i) =>{
+        let { offsetWidth, offsetLeft} = btn;
+            activeTabLineRef.current.style.width = offsetWidth + "px";
+            activeTabLineRef.current.style.left = offsetLeft + "px";
+
+            setInPageNavIndex(i);
     }
+
+    useEffect(()=>{
+        changePageState( activeTabRef.current, defaultActiveIndex); 
+    },[])
   return (
     <>
         <div className="relative mb-8 bg-white border-b border-grey flex flex-nowrap overflow-x-auto">
             {
                 routes.map((route,i)=>{
                     return (
-                        <button onClick ={ ()=>{
-                            changePageState()
-                        }} key={i} className={"p-4 px-5 capitalize" + (inPageNavIndex === i ? "text-black" : "text-dark-grey")}>
+                        <button ref={i === defaultActiveIndex ? activeTabRef : null} onClick ={ (e)=>{
+                            changePageState(e.target, i)
+                        }} key={i} className={"p-4 px-5 capitalize " + (inPageNavIndex == i ? "text-black" : "text-dark-grey ") + ( defaultHidden.includes(route) ? "hidden" : "")}>
                             {route}
-                        </button>
+                        </button>   
                     )
                 })
             }
 
-            <hr></hr>
+            <hr ref={activeTabLineRef}  className="absolute bottom-0 duration-300"></hr>
         </div>
-
+        { Array.isArray(children) ? children[inPageNavIndex] : children }
     </>
   )
 }
--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react"
 import AnimationWrapper from "../common/page-animation"
 import InPageNavigationComponent from "../components/inpage-navigation.component"
+import axios from "axios";
+import Loader from "../components/loader.component";
+import BlogPostCard from "../components/blog-post.component";
 
 const HomePage = () => {
+
+  const [blogs, setBlog] = useState(null);
+  const fetchLatestBlogs = () => {
+    axios.get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
+    .then(({ data })=>{
+      setBlog(data.blogs);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  useEffect(()=>{
+    fetchLatestBlogs();
+  },[])
   return (
     <AnimationWrapper>
         <section className="h-cover flex justify-center gap-10">
             <div className="w-full">
-                <InPageNavigationComponent routes = {["Home", "Trending blogs"]}>
-
+                <InPageNavigationComponent routes = {["home", "trending blogs"]} defaultHidden = {["trending blogs"]}>
+                  {/* <h1>Latest Blogs Here</h1> */}
+                  <>
+                    {
+                      blogs === null ? <Loader/> : 
+                      blogs.map((blog, i)=>{
+                          return (
+                            <AnimationWrapper transition={{ duration : 1, delay : i*.1}} key={i}>
+                              <BlogPostCard content = {blog} author = {blog.author.personal_info}/>
+                            </AnimationWrapper>
+                          )
+                      })
+                    }
+                  </>
+                  <h1>Trending Blogs Here</h1>
                 </InPageNavigationComponent>
             </div>
         </section>
